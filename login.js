13ecd8372f7977357e04c73849209062b25243c9
oONathanOo <146172392+oONathanOo@users.noreply.github.com>
2023-10-19 18:31:53 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-
 const correctPassword = "apple";
 
 const loginButton = document.getElementById("loginButton");
@@ -7,9 +6,9 @@ loginButton.addEventListener("click", function() {
     const passwordInput = document.getElementById("password").value;
 
     if (passwordInput === correctPassword) {
-   
         window.location.href = "new_page.html";
     } else {
         alert("Incorrect password. Please try again.");
     }
 });
+
